fa5bdc5edaa27d2a858efc8acafd99b562fbe793
Drozerah <drozerah@gmail.com>
2019-07-04 17:10:26 +0200

    refactor: remove default vue-cli stuff + add Title.vue component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title msg="Le Déradicaliseur"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Title from '@/components/Title.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>
