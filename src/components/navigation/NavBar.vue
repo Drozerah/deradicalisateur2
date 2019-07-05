759131dfdcce6490dad085eee7d4bbe14f5b61ff
Drozerah <drozerah@gmail.com>
2019-07-05 12:29:19 +0200

    feat: add components sub repository

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="white flex-container" role="navigation">
+    <ul>
+      <router-link
+        v-for='link in links'
+        :key='link.path'
+        :to='link.path'
+        tag="li"
+        active-class="active"
+        :class="wavesEffect"
+        :exact='link.path == "/" ? true : false'
+      >
+      <a>{{link.path == "/" ? pathHomeName : link.name | capitalize}}</a>
+      </router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      pathHomeName: '||||||||',
+      links: this.$router.options.routes,
+      wavesEffect: 'waves-effect waves-yellow' // CSS transition
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  ul{
+    li{
+      a{
+        color: #2c3e50;
+      }
+    }
+  }
+}
+</style>
