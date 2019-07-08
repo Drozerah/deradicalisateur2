307a8fe3b8b07b5276bd6ea46100c98319ec5130
Drozerah <drozerah@gmail.com>
2019-07-08 22:38:55 +0200

    feat: add link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="center">
       <img class="responsive-img" :alt="Title" src="@/assets/jpg/bandeau.jpg" v-if='DisplayLogo'>
     </div>
-    <h3>Footer</h3>
+    <div class="editor">{{copyright}} <a :href='editor.link' target="_blanck" rel="noopener">{{ editor.name }}</a></div>
   </footer>
 </template>
 
@@ -13,10 +13,33 @@ export default {
   props: {
     Title: String,
     DisplayLogo: Boolean
+  },
+  data () {
+    return {
+      editor: {
+        link: 'http://www.editions-rjtp.com/',
+        name: `- Les Editions - Réflechir n'a Jamais Tué Personne - R.J.T.P. -`
+      },
+      copyright: `Copyright ${new Date().getFullYear()}`
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+@import "@/styles/_variables.scss";
+.editor{
+  text-align: center;
+}
+a{
+  text-decoration: underline;
+  color: inherit;
+  padding: 5px;
+  &:hover{
+    background-color: #333;
+    /* Best accessibility contrast */
+    /* background-color:#010300; */
+    color: $primary;
+  }
+}
 </style>
