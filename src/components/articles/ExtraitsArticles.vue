322f85abea90d285bec4f06ab663e9fd52c9b120
Drozerah <drozerah@gmail.com>
2019-07-10 23:27:04 +0200

    feat: add extraits text content

--- src/components/articles/ExtraitsArticles.vue
+++ src/components/articles/ExtraitsArticles.vue
@@ -1,20 +1,27 @@
 <template>
-  <article>
+  <article :class="$mq">
     <header>
-      <h1>Extraits</h1>
+      <h1>Extraits:</h1>
     </header>
-    <div class="content">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quibusdam cum, soluta sapiente, dignissimos laboriosam, magni quaerat fugiat dolor aliquam ea architecto ipsam? Mollitia voluptatum delectus fugiat odit, nobis praesentium.
+    <div class="text-content">
+      <p>Bonjour,</p>
+      <p>Comme vous allez pouvoir le constater, <span class="bold">les extraits et les planches disponibles <span v-if="$mq =='desktop'">ci-contre</span><span v-else>ci-dessus</span> et sur ce site, ne comportent pas les bulles de texte</span>. Pourquoi ?
+      C'est une BD sans bulle ? Non, 94 % des cases en sont pourvues.<br>
+      Est-ce un oubli ?<br>
+      Est-ce par "fourberie" ? Oui et Non.</p>
+      <p>Nous avons choisi de ne pas mettre le texte parce qu'il serait forcément sorti de son contexte, et il serait difficile de saisir la progression, la stratégie du déradicaliseur.
+      Pour voir le texte correspondant à chaque image <span v-if="$mq =='desktop'">ci-contre</span><span v-else>ci-dessus</span>, il faudra donc aller chez votre libraire favori, et au pire, le commander sans savoir de quoi il en retourne. Mais rassurez vous, nous avons travaillé avec beaucoup d'application pour que vous ne regrettiez pas d'avoir cette BD entre les mains.</p>
+      <p>Comme pour la précédente BD "Joanne Lebster, le début d'un nouveau monde", réalisée par Mathieu et Marc, préparez-vous à aller où peu de BDs (de part leur theme) vous ont déjà emmené.
+      :)</p>
+      <b v-if="$mq == 'desktop'">
+        <br>
+          <div class="divider"></div>
+        <br>
+      </b>
+      <p>voir aussi:</p>
+      <div class="center">
+        <router-link to="entrevue" class="btn waves-effect waves-yellow grey lighten-1 black-text darken-text-2">Interview présentation de l'ouvrage</router-link>
+      </div>
     </div>
   </article>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style lang="scss" scoped>
-
-</style>
--- src/views/Extraits.vue
+++ src/views/Extraits.vue
@@ -1,7 +1,5 @@
 <template>
   <main class="main extraits">
-    <br>
-    <br>
     <Carousel/>
     <Article/>
     <mq-layout mq="tablet+">
@@ -25,5 +23,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main{
+  // background-color: orange;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  // padding: 20px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  // align-items: center;
+  aside{
+    // background-color: red;
+  }
+  article{
+    // background-color: #E7E7E7;
+    padding: 40px 45px 40px 45px;
+    width: 658px;
+    min-width: 658px;
+    // text-align: justify;
+  }
+}
+</style>
 
+<style lang="scss">
+main{
+  article{
+    .text-content{
+      font-size: 18px;
+    }
+    header{
+      h1{
+        margin: 0px 0px 40px 0px;
+        font-size: 25px;
+        color:gray;
+      }
+    }
+  }
+}
 </style>
