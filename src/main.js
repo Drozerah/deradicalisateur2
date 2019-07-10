3b9200842fee7f96fb838cc003d195b8fb23fc2a
Drozerah <drozerah@gmail.com>
2019-07-10 15:15:26 +0200

    chore: import vue-mq + breakpoints setting

--- src/main.js
+++ src/main.js
@@ -5,10 +5,22 @@ import 'materialize-css/dist/css/materialize.min.css'
 // import(/* webpackPreload: true */ "materialize-css/dist/css/materialize.min.css")
 // @doc /> https://medium.com/webpack/link-rel-prefetch-preload-in-webpack-51a52358f84c
 import Vue2Filters from 'vue2-filters'
-Vue.use(Vue2Filters)
+import VueMq from 'vue-mq'
 Vue.config.productionTip = false
+
+Vue.use(Vue2Filters)
+
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 600,
+    tablet: 992,
+    laptop: 1200,
+    desktop: Infinity
+  }
+})
 
 new Vue({
   router,
+  VueMq,
   render: h => h(App)
 }).$mount('#app')
