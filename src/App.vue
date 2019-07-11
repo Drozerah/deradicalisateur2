7101adfcbb020a754294e099fc2e58c7a87d4c04
Drozerah <drozerah@gmail.com>
2019-07-11 18:13:09 +0200

    style: remove default font-family + change color

--- src/App.vue
+++ src/App.vue
@@ -61,10 +61,10 @@ body {
   padding: 0px;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  // font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: 333;
 }
 h2{
   text-align: center;
