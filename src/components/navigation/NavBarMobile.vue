c77f3c367fd5150e9126f159f4842548b9638920
Drozerah <drozerah@gmail.com>
2019-07-11 18:53:10 +0200

    feat: add avatar to NavBarMobile.vue

--- src/components/navigation/NavBarMobile.vue
+++ src/components/navigation/NavBarMobile.vue
@@ -1,8 +1,14 @@
 <template>
   <!-- Mobile device sidenav conditional rendering -->
   <nav class="bg-yellow hide-on-med-and-up" role="navigation">
-    <div class="nav-wrapper ">
+    <div class="nav-wrapper bg-yellow">
       <ul id="nav-mobile" class="sidenav" ref="sidenav">
+        <li>
+          <div class="user-view">
+            <div class="avatar"><img class="circle" :src="src"></div>
+            <span class="black-text name center">Le Déradicaliseur</span>
+          </div>
+        </li>
         <router-link
           v-for='link in links'
           :key='link.path'
@@ -26,7 +32,8 @@ export default {
     return {
       pathHomeName: '||||||||',
       links: this.$router.options.routes.filter(link => link.meta.isPublicLink === true),
-      wavesEffect: 'sidenav-close waves-effect waves-yellow' // CSS transition
+      wavesEffect: 'sidenav-close waves-effect waves-yellow', // CSS transition
+      src: require('@/assets/jpg/avatar.jpg')
     }
   },
   mounted () {
@@ -43,8 +50,12 @@ export default {
 nav {
   ul{
     li{
+      .avatar{
+        display: flex;
+        justify-content: center;
+      }
       a{
-        color: #2c3e50;
+        color: #333;
       }
     }
   }
