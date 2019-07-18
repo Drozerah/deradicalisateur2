8c1e4d9cf7db3148bca6a931916133dc0af5c2a2
Drozerah <drozerah@gmail.com>
2019-07-18 20:56:46 +0200

    feat: add actualités components and assets

--- src/components/articles/ActualitesArticle.vue
+++ src/components/articles/ActualitesArticle.vue
@@ -4,21 +4,103 @@
       <h1>{{title}}</h1>
       <div class="content">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat suscipit vero autem alias facilis mollitia nostrum tempora sapiente consequuntur veritatis?</p>
+        <Spinner v-if="spinner"/>
+        <div v-else v-html="result">
+        </div>
       </div>
     </header>
   </article>
 </template>
 
 <script>
+// @ is an alias to /src
+import Spinner from '@/components/preloader/CircularSpinner.vue'
 export default {
+  components: {
+    Spinner
+  },
   data () {
     return {
-      title: 'Actualités'
+      title: 'Actualités',
+      spinner: true,
+      result: undefined
     }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      fetch('/../actu/index.html', { cache: 'no-store' })
+        .then(
+          res => {
+            return res.text()
+          }
+        )
+        .then(
+          data => {
+            console.log(data)
+            setTimeout(() => {
+              this.spinner = false
+              this.result = data
+            }, 3000)
+          }
+        )
+        .catch(error => console.error(error))
+    })
   }
 }
 </script>
-
 <style lang="scss" scoped>
+@import "@/scss/_variables.scss";
+@import "@/scss/_mixins.scss";
+/* MOBILE */
+article.mobile{
+  @include article-mobile-width; // mixin
+}
+/* TABLET - LAPTOP - DESKTOP */
+article.tablet, article.laptop, article.desktop{
+  @include article-tablet-and-up-width; // mixin
+}
+/* MOBILE - TABLET - LAPTOP - DESKTOP */
+article{
+  .content{
+    font-size: 20px;
+    margin-top: 60px;
+  }
+}
+</style>
 
+<style lang="scss">
+.content{
+  @import "@/scss/_variables.scss";
+  ul{
+    li{
+      // margin-bottom: 20px;
+      display: flex;
+      flex-direction: column;
+      .img-card{
+        align-self: center;
+        padding: 10px;
+        margin: 20px 0px;
+        display: inherit;
+        width: 500px;
+      }
+      p{
+        margin:0px;
+        padding-left: 0px;
+      }
+    }
+    li:before {
+      position: relative;
+      display: inherit;
+      top:29px;
+      left:-10px;
+      content: "label_important"; /* GoogleFont Icon */
+      font-family: 'Material Icons';
+      display: inline-block;
+      margin-left: -1.3em; /* same as padding-left set on li */
+      width: 1.3em; /* same as padding-left set on li */
+      color: $primary;
+      // color:gray;
+    }
+  }
+}
 </style>
---
+++ src/components/preloader/CircularSpinner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="circular-spinner">
+    <div class="preloader-wrapper big active">
+      <div class="spinner-layer spinner-yellow-only">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.circular-spinner{
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  padding: 10%;
+}
+</style>
